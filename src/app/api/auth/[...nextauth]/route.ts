6c66fb9cddc210267f852c7cccd73a0fe5b6e894
tensho1026 <tensho1026@icloud.com>
2025-07-02 03:25:10 +0900

    authoptionsを分離

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,39 +1,9 @@
 // app/api/auth/[...nextauth]/route.ts
 
-import NextAuth from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
-import { NextAuthOptions } from "next-auth";
+import { authOptions } from "@/app/utils/authOptions";
+import NextAuth from "next-auth/next";
 
 export const runtime = "nodejs";
 
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-      authorization: {
-        params: {
-          scope: "read:user user:email repo",
-        },
-      },
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, account }) {
-      if (account) {
-        token.accessToken = account.access_token; // ← GitHubトークンを保存
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      session.accessToken = token.accessToken as string;
-      return session;
-    },
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-};
-
 const handler = NextAuth(authOptions);
-
-export const GET = handler;
-export const POST = handler;
+export { handler as GET, handler as POST };
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/utils/authOptions";
 import AuthButton from "../auth/components/Auth-Button";
 import HomeScreen from "@/app/home/components/Home";
 
